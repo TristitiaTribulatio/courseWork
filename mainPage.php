35bf26c0b9506250f3c5893e406b915703371ef9
Владислав <PeinDefender@gmail.com>
2020-11-01 12:15:37 +0300

    add header

---
+++ mainPage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mainPage</title>
+    <link rel="stylesheet" href="/resources/css/index.css">
+    <link rel="stylesheet" href="/resources/css/header.css">
+</head>
+<body>
+    <? include "./resources/php/header.php"; ?>
+</body>
+</html>
---
+++ resources/php/header.php
@@ -0,0 +1,10 @@
+<header>
+    <a href="/mainPage.php" class="logo"><img src="/resources/imgs/logo.png"></a>
+    <a href="/mainPage.php" class="title"><h1>nameOfCollege</h1></a>
+    <ul class="links">
+        <li><a href="#"><img src="/resources/imgs/link1.png"></a></li>
+        <li><a href="#"><img src="/resources/imgs/link1.png"></a></li>
+        <li><a href="#"><img src="/resources/imgs/link1.png"></a></li>
+        <li><a href="#"><img src="/resources/imgs/link1.png"></a></li>
+    </ul>
+</header>
