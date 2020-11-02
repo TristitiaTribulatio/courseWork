6a54eb166e558c20b7c56ed7cd6b9e0a628e1d54
Владислав <PeinDefender@gmail.com>
2020-11-02 17:33:15 +0300

    add main&footer

--- mainPage.php
+++ mainPage.php
@@ -7,10 +7,35 @@
     <link rel="stylesheet" href="/resources/css/index.css">
     <link rel="stylesheet" href="/resources/css/header.css">
     <link rel="stylesheet" href="/resources/css/mainPage.css">
+    <link rel="stylesheet" href="/resources/css/footer.css">
 </head>
 <body>
     <? include "./resources/php/header.php"; ?>
     <main>
+        <section class="aboutUs">
+            <div class="aboutUsInfo">
+                <h2>ABOUT US</h2>
+                <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat incidunt suscipit dolor, inventore error eligendi deserunt perspiciatis sed ab explicabo minus esse consequatur possimus voluptatibus tempore aliquam culpa impedit eos.
+                Facere aliquid alias, perspiciatis repellat commodi deleniti rem dolorem, rerum ipsa quo accusantium voluptatum eos nulla a sapiente tempore fugiat laboriosam in earum incidunt ad! Perferendis incidunt expedita nesciunt aliquid.
+                Iure dicta amet placeat eligendi ipsum laudantium soluta labore autem assumenda, ad laboriosam beatae, earum repellat quia aut libero esse delectus. Nam quasi perspiciatis tenetur nemo sequi fugiat officiis laudantium!
+                Quam eius facere dolor totam ut illum, ratione neque modi quas, odit aut animi asperiores, corporis quod! Sit cum eius officiis inventore maxime, excepturi, ullam illum voluptatem quam itaque eveniet?
+                Sint maiores, molestias consequuntur minima quisquam earum officiis dolore expedita aspernatur quod facilis temporibus architecto officia veritatis nulla fugit mollitia consectetur. At aliquid cum dolores vitae impedit inventore, cupiditate reiciendis!
+                </p>
+            </div>
+        </section>
+        <section class="itemsMenu">
+            <ul class="items">
+                <li><a href="#"><img src="/resources/imgs/reviewIcon.png"></a></li>
+                <li><a href="#"><img src="/resources/imgs/newsIcon.png"></a></li>
+                <li><a href="#"><img src="/resources/imgs/historyIcon.png"></a></li>
+            </ul>
+        </section>
+        <section class="gallery">
+            <ul class="imgs">
+            </ul>
+        </section>
+        <? include "./resources/php/footer.php";  ?>
     </main>
 </body>
 </html>
---
+++ resources/php/footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <ul class="footer">
+        <li>
+            <img src="/resources/imgs/locationIcon.png">
+            <p>10100, г. Москва, ул. Центральная, д. 1</p>
+        </li>
+        <li>
+            <img src="/resources/imgs/phoneIcon.png">
+            <p>+7(999)999-99-99</p>
+        </li>
+        <li>
+            <img src="/resources/imgs/mailIcon.png">
+            <p>college@college.com</p>
+        </li>
+    </ul> 
+</footer>
