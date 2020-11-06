10adb43f79fb13ce15f1e9c3b9fd31b0afe795cc
Владислав <PeinDefender@gmail.com>
2020-11-06 16:14:18 +0300

    add adminPage

--- adminPage.php
+++ adminPage.php
@@ -17,6 +17,15 @@
 </head>
 <body>
     <? include "./resources/php/header.php"; ?>
+    <main>
+        <section class="adminPanel">
+            <h2>Вы авторизированы, как <?= $_SESSION["name"]; ?></h2>
+            <ul>
+                <li><a href="/resources/php/controlReviews.php">КОНТРОЛЬ ОТЗЫВОВ</a></li>
+                <li><a href="/resources/php/addNews.php">ДОБАВИТЬ НОВОСТЬ</a></li>
+            </ul>
+        </section>
+    </main>
     <? include "./resources/php/footer.php"; ?>
     <script src="/resources/js/adminPage.js"></script>
 </body>
